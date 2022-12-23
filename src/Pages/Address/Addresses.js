5e6dc77fb219d0451e69ba0c8afed97bb02fcfb9
mickeymaruf <mickeymaruf@gmail.com>
2022-12-23 15:56:43 +0600

    addresses page pagination and loader spinner added

--- src/Pages/Address/Addresses.js
+++ src/Pages/Address/Addresses.js
@@ -1,21 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import PageSpinner from '../../components/PageSpinner';
+import Pagination from '../../components/Pagination';
 import Address from './Address';
 
 const Addresses = () => {
     const [addresses, setAddresses] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(16);
+
     useEffect(() => {
+        setLoading(true);
         fetch('https://api.spacexdata.com/v3/payloads')
             .then(res => res.json())
-            .then(data => setAddresses(data));
+            .then(data => {
+                setAddresses(data);
+                setLoading(false);
+            });
     }, [])
+
+    // Get current items
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = addresses.slice(indexOfFirstItem, indexOfLastItem);
+
+    if (loading) {
+        return <PageSpinner />
+    }
+
     return (
-        <section className='w-11/12 mx-auto pb-10 pt-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
-            {
-                addresses.map((address, idx) => <Address
-                    key={idx}
-                    address={address}
-                ></Address>)
-            }
+        <section className='w-11/12 mx-auto pb-10 pt-1'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+                {
+                    currentItems.map((address, idx) => <Address
+                        key={idx}
+                        address={address}
+                    ></Address>)
+                }
+            </div>
+            <Pagination
+                itemsPerPage={itemsPerPage}
+                totalItems={addresses.length}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+            />
         </section>
     );
 };
